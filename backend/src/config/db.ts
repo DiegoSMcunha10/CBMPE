4626a68261ee9c5f5b7d96259d0551ce3d15a864
DiegoSMcunha10 <diegosmcunha10@gmail.com>
2025-10-28 17:46:58 -0300

    removendo link do banco

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function conexaoMongoDb() {
   try {
-    await mongoose.connect("mongodb+srv://teste:m6iMH0LYJBA7fDq1@cluster0.fjicpcq.mongodb.net/bancocbmpe");
+    await mongoose.connect("");
     console.log("✅ Conectado com sucesso!");
   } catch (error) {
     console.error("❌ Erro ao conectar:", error);
